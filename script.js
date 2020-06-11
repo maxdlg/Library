83d1bcf11c20f9c673be25ab23ecdc06c62b6762
Max G <gmax0687@gmail.com>
2020-06-11 14:56:47 -0500

    added basic functionality

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return title + " by " + author + ", " + pages + " pages, read: " + read
-    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary()
+Book.prototype.info = function () {
+    return (
+        this.title +
+        " by " +
+        this.author +
+        ", " +
+        this.pages +
+        " pages, read: " +
+        this.read
+    );
+};
+
+function addBookToLibrary(title, author, pages, read) {
+    book = Object.create(Book.prototype);
+    book.title = title;
+    book.author = author;
+    book.pages = pages;
+    book.read = read;
+    myLibrary.push(book);
+}
+
+addBookToLibrary("bruh", "nug", 420, false);
+addBookToLibrary("frick", "nig", 69, true);
+console.log(book.info);
+
+const main = document.querySelector("main");
+function render() {
+    myLibrary.forEach((shit) => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        main.appendChild(card);
+        card.textContent = shit.info();
+        console.log(shit);
+    });
+}
+
+render();
+
+console.log(myLibrary);
