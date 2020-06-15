b1de3d010f1a40613f423520a37d20006b4b0afe
Max G <gmax0687@gmail.com>
2020-06-15 18:23:45 -0500

    made it look pretty

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 let myLibrary = [];
+const cardHolder = document.querySelector(".cardHolder");
+const addNew = document.getElementById("addNew");
+addNew.addEventListener("click", hideForm);
+const form = document.querySelector("form");
+const submit = document.getElementById("submit");
+submit.addEventListener("click", hideForm);
 
-function Book(title, author, pages, read) {
+let hid = true;
+
+function hideForm() {
+    if (hid === true) {
+        form.style.right = "65px";
+        addNew.style.rotate = "45deg";
+        addNew.style.backgroundColor = "#ff4500";
+        hid = false;
+    } else {
+        form.style.right = "-350px";
+        addNew.style.rotate = "0deg";
+        addNew.style.backgroundColor = "#5ac1ff";
+        hid = true;
+    }
+}
+
+function bookCreator(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-Book.prototype.info = function () {
+bookCreator.prototype.info = function () {
     return (
         this.title +
         " by " +
@@ -19,30 +41,28 @@ Book.prototype.info = function () {
     );
 };
 
-function addBookToLibrary(title, author, pages, read) {
-    book = Object.create(Book.prototype);
-    book.title = title;
-    book.author = author;
-    book.pages = pages;
-    book.read = read;
-    myLibrary.push(book);
+function addBookToLibrary(bookTitle, author, pages, read) {
+    title = bookTitle;
+    title = Object.create(bookCreator.prototype);
+    title.title = bookTitle;
+    title.author = author;
+    title.pages = pages;
+    title.read = read;
+    myLibrary.push(title);
+    render();
 }
 
 addBookToLibrary("bruh", "nug", 420, false);
 addBookToLibrary("frick", "nig", 69, true);
-console.log(book.info);
+addBookToLibrary("black", "bad", 343, true);
 
-const main = document.querySelector("main");
 function render() {
-    myLibrary.forEach((shit) => {
-        let card = document.createElement("div");
+    let card = document.createElement("div");
+    myLibrary.forEach((book) => {
         card.classList.add("card");
-        main.appendChild(card);
-        card.textContent = shit.info();
-        console.log(shit);
+        cardHolder.appendChild(card);
+        card.textContent = book.title + "\n" + book.author;
     });
 }
 
-render();
-
-console.log(myLibrary);
+console.table(myLibrary);
